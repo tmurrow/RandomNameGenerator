202adafc41a5cc873eb9cdf91d60dd9f9825fc39
Tanya <tanya.murrow@hobbylobby.com>
2016-12-19 09:32:42 -0600

    First commit

---
+++ RandomNameGenerator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(Console.WindowWidth, 40);
+
+            Console.WriteLine("Names:\n");
+
+            List<string> prefixes = new List<string>();
+            List<string> suffixes = new List<string>();
+
+            StreamReader prefixFile = new StreamReader("prefixes.txt");
+            StreamReader suffixFile = new StreamReader("suffixes.txt");
+            string line;
+
+            while ((line = prefixFile.ReadLine()) != null)
+            {
+                prefixes.Add(line);
+            }
+
+            prefixFile.Close();
+
+            while ((line = suffixFile.ReadLine()) != null)
+            {
+                suffixes.Add(line);
+            }
+
+            suffixFile.Close();
+
+            do
+            {
+                Random r = new Random();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    int prefix = r.Next(prefixes.Count);
+                    int suffix = r.Next(suffixes.Count);
+                    string name = prefixes.ElementAt(prefix) + suffixes.ElementAt(suffix);
+                    Console.WriteLine(name);
+                }
+
+                Console.WriteLine("\nPress Enter for more names, any other key to exit...\n");
+            } while (Console.ReadKey().Key == ConsoleKey.Enter);
+        }
+    }
+}
---
+++ RandomNameGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RandomNameGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RandomNameGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55eb09cf-f319-42c6-9a5e-7434d6be73a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
